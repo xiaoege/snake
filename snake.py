e48b965534c0f6f3259815e36b941bc5e38455eb
ChenHang <237113730@qq.com>
2020-07-09 11:34:51 +0800

    抓取新闻保存本地

--- snake.py
+++ snake.py
@@ -2,17 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 import pymysql.cursors
 import uuid0
+import os
+import time
+import random
 
 # url = 'http://www.chinadaily.com.cn/a/202007/07/WS5f03dd80a310834817257acd.html'
 # url = 'http://www.chinadaily.com.cn/a/202006/29/WS5ef9598fa310834817255c98.html'
 url = 'http://www.chinadaily.com.cn/a/202007/08/WS5f03d442a310834817257a4c.html'
 
+header = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
+
 
 def download_page():
-    header = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
     r = requests.get(url, headers=header)
-    return r.text
+    if r.status_code == 200:
+        return r.text
+    else:
+        raise BaseException
 
 
 def parse():
@@ -39,18 +46,38 @@ def parse():
     # 新闻内容
     content = soup.find(id='Content')
     contents = content.contents
+    figures = content.find_all('figure')
+
+    # 图片
+    figure_list = []
+    for i in figures:
+        img = i.find('img')['src']
+        img = 'http:' + img
+        r = requests.get(img, headers=header, stream=True)
+        if r.status_code == 200:
+            file_path = mkdir() + '.jpeg'
+            # _file = 
+            with open(file_path, 'wb') as f:
+                f.write(r.content)
+
+        figcaption = i.find('figcaption').string.strip()
+        figure_list.append(img)
+        figure_list.append(figcaption)
+
+    # 文字
     content_list = []
     for i in contents:
         if i != '\n':
-            content_list.append(i)
-
+            if 'figure' in str(i):
+                pass
+            else:
+                content_list.append(i)
 
     # 新闻页数
     div_currpage = soup.find(id='div_currpage')
     if div_currpage != None:
         pages = div_currpage.find_all('a', class_=None)
         for i in pages:
-            print(type(i))
             print(i['href'])
 
     connection = pymysql.connect(host='localhost',
@@ -62,16 +89,32 @@ def parse():
 
     with connection.cursor() as cursor:
         uuid = str(uuid0.generate())
-        sql = "insert into rtc_news(uuid,author,title,content,source)values(%s,%s,%s,%s,%s)"
-        cursor.execute(sql,(uuid,_author,str(title),str(contentList),'Chinadaily'))
+        # sql = "insert into rtc_news(uuid,author,title,content,source)values(%s,%s,%s,%s,%s)"
+        # cursor.execute(sql, (uuid, _author, str(title),
+        #  str(contentList), 'Chinadaily'))
         # cursor.execute(sql,('uuid','_author','title','content','Chinadaily'))
         # result = cursor.fetchone()
-        connection.commit()
+        # connection.commit()
 
     connection.close()
 
     print('aaa')
 
 
+def mkdir():
+    _path = '/Users/chenhang/work/picture/chinadaily/'
+    _month = time.strftime("%Y-%m", time.localtime())
+    _day = time.strftime("%d", time.localtime())
+    if os.path.exists(_path + _month + '/' + _day):
+        pass
+    else:
+        os.makedirs(_path + _month + '/' + _day)
+    _timestamp = time.mktime(time.strptime(time.strftime("%a %b %d %H:%M:%S %Y", time.localtime()),"%a %b %d %H:%M:%S %Y"))
+    file_name = str(_timestamp)[:-2] + '-' + str(random.randint(0,10000))
+    # 不带.jpg /.png之类
+    return _path + _month + '/' + _day + '/' + file_name
+
+
 if __name__ == "__main__":
+    mkdir()
     parse()
